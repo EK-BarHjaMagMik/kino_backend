b5cd7c62c5f550ba8873c1ff49e4711efd2544ed
Mikkel N. Broholm <mibr0005@stud.ek.dk>
2026-03-12 16:31:33 +0100

    remove create showing functionality from `ShowingController`

--- src/main/java/org/example/kino_backend/controller/ShowingController.java
+++ src/main/java/org/example/kino_backend/controller/ShowingController.java
@@ -1,11 +1,8 @@
 package org.example.kino_backend.controller;
 
-import org.example.kino_backend.dto.CreateShowingRequest;
 import org.example.kino_backend.dto.ShowingDTO;
 import org.example.kino_backend.dto.ShowingSeatDTO;
-import org.example.kino_backend.model.Showing;
 import org.example.kino_backend.service.ShowingService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -42,13 +39,5 @@ public class ShowingController {
     @GetMapping("/{id}/seats")
     public ResponseEntity<List<ShowingSeatDTO>> getSeats(@PathVariable Long id) {
         return ResponseEntity.ok(showingService.getSeatsForShowing(id));
-    }
-
-    @PostMapping
-    public ResponseEntity<ShowingDTO> create(@RequestBody CreateShowingRequest req) {
-        Showing showing = showingService.create(req);
-        ShowingDTO dto = ShowingDTO.fromEntity(showing);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(dto);
     }
 }
